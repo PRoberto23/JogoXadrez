6082404eeb3731c7be8dd6d754ea5bb32980ae0c
Paulo Roberto <paulocarlos2468@gmail.com>
2023-01-02 18:54:09 -0300

    add metodos gets and sets

--- src/com/tabuleiro/Tabuleiro.java
+++ src/com/tabuleiro/Tabuleiro.java
@@ -3,13 +3,11 @@ package com.tabuleiro;
 public class Tabuleiro {
 	private Integer linhas;
 	private Integer colunas;
-	
+
 	private Peca[][] pecas;
 
-	
 	/*
-	 * Iniciar as variaveis como null no contrutor padrão
-	 * para evitar erros
+	 * Iniciar as variaveis como null no contrutor padrão para evitar erros
 	 */
 	public Tabuleiro() {
 		linhas = null;
@@ -22,6 +20,25 @@ public class Tabuleiro {
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
 	}
+
+	public Integer getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(Integer linhas) {
+		this.linhas = linhas;
+	}
+
+	public Integer getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(Integer colunas) {
+		this.colunas = colunas;
+	}
 	
-	
+	public void lugarPeca(Peca peca, Posicao posicao) {
+		
+	}
+
 }
